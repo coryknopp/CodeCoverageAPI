316fab1634a370cf6b048dd7d3f3df944710340c
Justin Alvin <jnalviano@gmail.com>
2017-11-10 17:02:19 -0500

    Basic coverage data deserialization implementation added.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import net.sourceforge.cobertura.coveragedata.ClassData;
+import net.sourceforge.cobertura.coveragedata.CoverageDataFileHandler;
+import net.sourceforge.cobertura.coveragedata.ProjectData;
+
+import java.io.File;
+
 public class Main {
 
-    public static void main(String... args) {
-        System.out.println("Coverage API");
+    public static void main(String[] args) {
+        deserialize();
+    }
+
+    private static void deserialize() {
+        // Loads serialized data file, deserializes using built-in Cobertura method and prints class names in file. All
+        // is now accessible, it's just a matter of determining what data we need and how to best store it.
+        ProjectData project = CoverageDataFileHandler.loadCoverageData(new File("triangle/cobertura.ser"));
+        for (Object obj : project.getClasses()) {
+            ClassData classData = (ClassData) obj;
+            System.out.println(classData.getName());
+        }
     }
 }
