af7cba870246484d89851abd2fbfc3129a730f9f
coryknopp <cknopp@umass.edu>
2017-11-15 13:22:02 -0500

    Temporarily removed 'testMethod' from interface methods. This will be added back in once more basic funcitonality has been achieved.

--- src/data_model/CoverageData.java
+++ src/data_model/CoverageData.java
@@ -3,7 +3,7 @@ package data_model;
 public interface CoverageData {
 
     void addTestMethod(TestMethod testMethod);
-    void addLine(TestMethod testMethod, Line line);
-    void addBranch(TestMethod testMethod, Branch branch);
-    void addCondition(TestMethod testMethod, Condition condition);
+    void addLine(Line line);
+    void addBranch(Branch branch);
+    void addCondition(Condition condition);
 }
