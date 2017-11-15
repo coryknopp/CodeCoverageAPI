b31a3e3a667729a673380c42c4a9f3e7a7ea74df
coryknopp <cknopp@umass.edu>
2017-11-15 13:20:54 -0500

    Defined constructors for line and branch classes

--- src/data_model/Branch.java
+++ src/data_model/Branch.java
@@ -1,4 +1,12 @@
 package data_model;
 
 public class Branch {
+
+  private String branchName;
+
+  public Branch(String branchName) {
+    this.branchName = branchName;
+  }
+
+
 }
--- src/data_model/Line.java
+++ src/data_model/Line.java
@@ -1,4 +1,21 @@
 package data_model;
 
 public class Line {
+
+  private int lineNumber;
+  private boolean isCovered;
+
+  public Line(int lineNumber, boolean isCovered) {
+    this.lineNumber = lineNumber;
+    this.isCovered = isCovered;
+  }
+
+  public int lineNumber() {
+    return lineNumber;
+  }
+
+  public boolean isCovered() {
+    return isCovered;
+  }
+
 }
