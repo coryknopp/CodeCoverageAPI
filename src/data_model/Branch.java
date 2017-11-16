b48a81519f317a9c2692938a2c88c72afea1d2ad
Justin Alvin <jnalviano@gmail.com>
2017-11-16 13:12:48 -0500

    Data model abstraction added

--- src/data_model/Branch.java
+++ src/data_model/Branch.java
@@ -3,34 +3,40 @@ import java.util.*;
 
 public class Branch {
 
-  private String className;
-  private int lineNumber;
-  private boolean isCovered;
-  private ArrayList<Boolean> conditions;
+    private Line line;
+    private String className;
+    private int lineNumber;
+    private boolean isCovered;
+    private ArrayList<Boolean> conditions;
 
-  public Branch(String className, int lineNumber, boolean isCovered, ArrayList<Boolean> conditions) {
-    this.className = className;
-    this.lineNumber = lineNumber;
-    this.isCovered = isCovered;
-    this.conditions = conditions;
-  }
+    public Branch(String className, int lineNumber, boolean isCovered, ArrayList<Boolean> conditions) {
+        this.className = className;
+        this.lineNumber = lineNumber;
+        this.isCovered = isCovered;
+        this.conditions = conditions;
+    }
 
-  public String className() {
-    return className;
-  }
+    public Branch(Line line) {
+        this.line = line;
+    }
 
-  public int lineNumber() {
-    return lineNumber;
-  }
+    public String className() {
+        return className;
+    }
 
-  public boolean isCovered() {
-    return isCovered;
-  }
+    public int lineNumber() {
+        return lineNumber;
+    }
 
-  public ArrayList<Boolean> conditions() {
-    return conditions;
+    public boolean isCovered() {
+        return isCovered;
+    }
 
-  }
+    public ArrayList<Boolean> conditions() {
+        return conditions;
+    }
 
-
+    public Line getLine() {
+        return line;
+    }
 }
--- src/data_model/Line.java
+++ src/data_model/Line.java
@@ -2,32 +2,45 @@ package data_model;
 
 public class Line {
 
-  private String className;
-  private int lineNumber;
-  private boolean isCovered;
-  private boolean isBranch;
+    private String className;
+    private int lineNumber;
+    private boolean isCovered;
+    private boolean isBranch;
+    private ProjectMethod method;
 
-  public Line(String className, int lineNumber, boolean isCovered, boolean isBranch) {
-    this.className  = className;
-    this.lineNumber = lineNumber;
-    this.isCovered  = isCovered;
-    this.isBranch   = isBranch;
-  }
+    public Line(String className, int lineNumber, boolean isCovered, boolean isBranch) {
+        this.className  = className;
+        this.lineNumber = lineNumber;
+        this.isCovered  = isCovered;
+        this.isBranch   = isBranch;
+    }
 
-  public String className() {
-    return className;
-  }
+    public Line(int lineNumber, ProjectMethod method) {
+        this.lineNumber = lineNumber;
+        this.method = method;
+    }
 
-  public int lineNumber() {
-    return lineNumber;
-  }
+    public String className() {
+        return className;
+    }
 
-  public boolean isCovered() {
-    return isCovered;
-  }
+    public int lineNumber() {
+        return lineNumber;
+    }
+
+    public boolean isCovered() {
+        return isCovered;
+    }
   
-  public boolean isBranch(){
-    return isBranch;
-  }
+    public boolean isBranch(){
+        return isBranch;
+    }
 
+    public int getLineNumber() {
+        return lineNumber;
+    }
+
+    public ProjectMethod method() {
+        return method;
+    }
 }
