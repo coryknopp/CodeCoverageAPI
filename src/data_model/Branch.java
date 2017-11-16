677524f248a399a702c2a2b282e25dd71035475a
coryknopp <cknopp@umass.edu>
2017-11-16 11:34:32 -0500

    data model updates

--- src/data_model/Branch.java
+++ src/data_model/Branch.java
@@ -2,10 +2,26 @@ package data_model;
 
 public class Branch {
 
-  private String branchName;
+  private String className;
+  private int lineNumber;
+  private boolean isCovered;
 
-  public Branch(String branchName) {
-    this.branchName = branchName;
+  public Branch(String className, int lineNumber, boolean isCovered) {
+    this.className = className;
+    this.lineNumber = lineNumber;
+    this.isCovered = isCovered;
+  }
+
+  public String className() {
+    return className;
+  }
+
+  public int lineNumber() {
+    return lineNumber;
+  }
+
+  public boolean isCovered() {
+    return isCovered;
   }
 
 
