4b7a7ea76e2cdaa2320bb29a466edb644b1648f9
Justin Alvin <jnalviano@gmail.com>
2017-11-27 13:07:56 -0500

    Added new build script.

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo
+echo "=================================================================="
+echo "Compiling coverage API..."
+echo "=================================================================="
+echo
+ant -buildfile api.xml clean compile
+
+echo
+echo "=================================================================="
+echo "Compiling coverage API tests..."
+echo "=================================================================="
+echo
+ant -buildfile api.xml compile.tests
+
+echo
+echo "=================================================================="
+echo "Running coverage API tests..."
+echo "=================================================================="
+echo
+ant -buildfile api.xml test
+
+echo
+echo "=================================================================="
+echo "Running coverage API..."
+echo "=================================================================="
+echo
+ant -buildfile api.xml run
