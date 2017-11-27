db958ace738933aae379d62529cb8c245cc5801d
Justin Alvin <jnalviano@gmail.com>
2017-11-27 11:45:55 -0500

    Added build script for Cobertura and API

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CodeCoverageAPI
+
+To execute coverage API, run `sh build.sh`. This will run Cobertura to get coverage data and then run coverage API.
--- build.sh
+++ build.sh
@@ -1,1 +1,36 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+echo
+echo "=================================================================="
+echo "Running Cobertura..."
+echo "=================================================================="
+echo
+ant -buildfile coverage.xml coverage
+
+echo
+echo "=================================================================="
+echo "Compiling coverage API..."
+echo "=================================================================="
+echo
+ant -buildfile api.xml clean compile
+
+echo
+echo "=================================================================="
+echo "Compiling coverage API tests..."
+echo "=================================================================="
+echo
+ant -buildfile api.xml compile.tests
+
+echo
+echo "=================================================================="
+echo "Running coverage API tests..."
+echo "=================================================================="
+echo
+ant -buildfile api.xml test
+
+echo
+echo "=================================================================="
+echo "Running coverage API..."
+echo "=================================================================="
+echo
+ant -buildfile api.xml run
